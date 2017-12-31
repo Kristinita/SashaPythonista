b6c6bc6ed9b72cd1087a79a90dc327763f025e68
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2017-12-31 17:07:07 +0300

    [Test] Appveyor for filenames

--- blabla.py
+++ blabla.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 # @Author: SashaChernykh
 # @Date: 2017-12-31 15:43:57
-# @Last Modified time: 2017-12-31 16:46:30
+# @Last Modified time: 2017-12-31 17:06:05
 # Check, if blabla in OS
 
+import glob
 import os
+
+globus = glob.glob('*.txt')
 
 # Get current directory
 # https://stackoverflow.com/a/5137507/5951529
@@ -12,7 +15,9 @@ currentdirectory = os.getcwd()
 
 # Get list all filenames in a directory
 # https://stackoverflow.com/a/1120736/5951529
-for filename in os.listdir(currentdirectory):
+
+
+for filename in globus:
 
     # Check if string in a file
     # https://stackoverflow.com/a/4944929/5951529
