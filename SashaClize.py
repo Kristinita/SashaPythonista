4f0cb16ded687fac9a24c5b8ecb0e5e5cd7a0906
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-01-02 10:09:54 +0300

    [Test] Remove SashaClize error

--- SashaClize.py
+++ SashaClize.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 # @Author: Kristinita
 # @Date: 2018-01-01 21:38:33
-# @Last Modified time: 2018-01-02 08:18:24
+# @Last Modified time: 2018-01-02 10:08:56
 from clize import run
+
+"""Docsting.
+
+Test
+"""
 
 
 def echo(word):
     return word
+
 
 if __name__ == '__main__':
     run(echo)
