a65f1595877d021ecb60c1b09139cb6e82c7fd5b
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-01-03 22:49:05 +0300

    [Fix] 2 errors

--- SashaLogbook.py
+++ SashaLogbook.py
@@ -9,6 +9,7 @@ log = logbook.Logger("Sasha Logbook")
 def test(*, level_logger: 'll'="logbook.StreamHandler(sys.stdout, level=logbook.ERROR).push_application()"):
     print(level_logger)
 
+
 log.debug("Debug message")
 log.warning("Warning message")
 log.error("Error message")
