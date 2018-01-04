bc4fd0ad0ed48834cb774d98dd35467dee4335e8
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-01-04 11:37:59 +0300

    [Test] Body error

--- tests/eric_config.py
+++ tests/eric_config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Author: Kristinita
 # @Date: 2018-01-02 08:35:59
-# @Last Modified time: 2018-01-03 19:34:48
+# @Last Modified time: 2018-01-04 11:37:11
 """Configuration file.
 
 Imports and variables.
@@ -19,7 +19,7 @@ import sys
 
 
 logbook.StreamHandler(sys.stdout,
-                      level=logbook.NOTICE).push_application()
+                      level=logbook.DEBUG).push_application()
 log = logbook.Logger("Sasha Logbook")
 
 # Get all .txt file in a directory
