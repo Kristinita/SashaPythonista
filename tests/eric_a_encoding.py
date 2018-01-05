354a76e7cc822ad0bbb49a5777f404e5857be2d7
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-01-05 14:30:29 +0300

    [Test] Green bold foreground

--- tests/eric_a_encoding.py
+++ tests/eric_a_encoding.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Author: Kristinita
 # @Date: 2018-01-02 09:40:46
-# @Last Modified time: 2018-01-05 14:25:39
+# @Last Modified time: 2018-01-05 14:29:47
 """Encoding checker.
 
 Check, that files in Windows-1251 encoding.
@@ -63,15 +63,15 @@ for filename in all_txt_in_eric_room_wihtout_subfolders:
                                       fileencoding +
                                       ", not in Windows-1251 encoding! Please, save " +
                                       filename_without_path + " in Windows-1251 encoding."))
-        log.notice(pyfancy().green("If encoding of file " + filename_without_path +
-                                   " is UTF-8 and you see message above for local wwtd testing, " +
-                                   filename_without_path +
-                                   " automatically will converted from UTF-8 to Windows-1251."))
+        log.notice(pyfancy().green().bold("If encoding of file " + filename_without_path +
+                                          " is UTF-8 and you see message above for local wwtd testing, " +
+                                          filename_without_path +
+                                          " automatically will converted from UTF-8 to Windows-1251."))
         encoding_failure_tests = True
 
 
 if encoding_failure_tests:
-    log.critical(pyfancy().red().bold(
+    log.critical(pyfancy().red_bg(
         "One or more your files not in Windows-1251 encoding. Please, convert it (them) to Windows-1251."))
 
 if not encoding_failure_tests:
